456a354c9792f1f452d24ed23a944a57e4f69ac8
Juan Melnechuk <85077913+SSxArg@users.noreply.github.com>
2021-10-26 20:55:39 -0300

    Update sketch_oct20a.ino

--- sketch_oct20a.ino
+++ sketch_oct20a.ino
@@ -28,38 +28,70 @@ Nuñez Lucas
 #include <TimerOne.h> //Foreach
 #include <NewPing.h>
 
-TM1637Display display(9, 10);
-
-
+//Conexiones
+const byte triggerUS = 12;
+const byte echoUS = 11;
+const byte dioTM = 10;
+const byte clkTM = 9;
+const byte buz = 7;
+const byte engine = 6;
+const byte green = 5;
+const byte red = 4;
+const byte reps = 10;
+//Libraries cfig
+TM1637Display display(clkTM, dioTM);
 Adafruit_MLX90614 mlx = Adafruit_MLX90614();
+NewPing sonar(triggerUS, echoUS, 200); //Trigger, Echo y Distancia
 
-NewPing sonar(12, 11, 200); //Trigger, Echo y Distancia
 int uS, dis;
 
 void setup() //Iniciamos la placa y sus componentes
 {
     Serial.begin(9600);
+    mlx.begin();
     display.setBrightness(5);
+    pinMode(buz, OUTPUT);
+    pinMode(red, OUTPUT);
+    pinMode(green, OUTPUT);
 }
-void loop() {
-  int uS = sonar.ping_median() / US_ROUNDTRIP_CM;
-  if (uS < 20 && uS > 1)
-  {
-    Serial.print("llego");
-    float temp = mlx.readObjectTempC();
-    display.showNumberDec(temp);
-    
-  }
-  delay(300);
+void loop()
+{                 
+    uS = sonar.ping_median();
+    dis = uS / US_ROUNDTRIP_CM;
+    if(dis < 25 && dis > 1){
+        float t = 0;
+        for(int i = 0; i < reps+1; i++){
+          t+=mlx.readObjectTempC();
+        }
+        display.setBrightness(5);
+        float temp = t/10;
+        display.showNumberDec(temp);
+        //Expulsar gel > Mover el motor
+        if(temp > 40){
+          
+          if(temp > 150){
+            Serial.print("MLX mal conectado");
+            digitalWrite(buz, HIGH);
+            delay(1500);
+            digitalWrite(buz, LOW);
+          }
+          display.showNumberDec(1111);
+          digitalWrite(red, HIGH); //Led rojo
+          for(int i = 0; i < 6; i++){
+            digitalWrite(buz, HIGH); //Buzzer 
+            delay(100);
+            digitalWrite(buz, LOW); //Buzzer 
+            delay(50);
+          }
+          digitalWrite(red, LOW);
+        } else {
+          digitalWrite(green, HIGH); //Led verde
+          delay(1500);
+          digitalWrite(green, LOW);
+        }
+        display.setBrightness(0);
+        delay(2500);
+    }
+    display.showNumberDec(0000);
+    delay(100);
 }
-
-
-
-
-
-
-
-
-
-
-  
